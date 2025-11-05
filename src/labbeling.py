7045d9568d3212ce471ead6e5cc320174bf202d6
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2025-11-05 21:11:43 +0700

    A src labbeling

---
+++ src/labbeling.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+# Path ke file CSV yang ingin diperbarui
+file_path = r"E:\DragonEye\dataset\graded_features.csv"
+
+# Baca data dari file CSV
+df = pd.read_csv(file_path)
+
+# Periksa kolom yang ada di DataFrame
+print("Kolom yang ada dalam DataFrame:", df.columns)
+
+# Menambahkan kolom 'label_asli' berdasarkan awalan nama file
+# Logika: 'MT1_' -> 'A', 'MT2_' -> 'B', 'MT3_' -> 'C'
+def assign_label(filename):
+    if filename.startswith('MT1'):
+        return 'A'
+    elif filename.startswith('MT2'):
+        return 'B'
+    elif filename.startswith('MT3'):
+        return 'C'
+    else:
+        return 'Unknown'  # Jika nilai tidak sesuai, kembalikan 'Unknown'
+
+# Terapkan fungsi untuk menambahkan kolom 'label_asli' berdasarkan nama file
+df['label_asli'] = df['filename'].apply(assign_label)
+
+# Simpan DataFrame kembali ke file CSV
+df.to_csv(file_path, index=False, encoding='utf-8')
+
+print(f"[INFO] Kolom 'label_asli' berhasil ditambahkan dan file disimpan ke {file_path}")
