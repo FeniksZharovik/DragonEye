29b2dcf1ae617d72f837cc30663c421f93ec2628
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2025-11-13 23:39:31 +0700

    M src pre processing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,8 +2,7 @@ import cv2
 import numpy as np
 
 def preprocess_image(img):
-    """Resize, denoise, dan konversi warna"""
-    img = cv2.resize(img, (256, 256))
+    """Denoise dan konversi warna, tanpa resize (untuk pengukuran akurat)."""
     img = cv2.GaussianBlur(img, (3, 3), 0)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     return hsv
